25d88a60eda4a474ec4ac106757650e623d9d51e
holdlemon <d.vajov2010@yandex.ru>
2024-12-10 21:35:54 +0300

    Внесены дополнения в серилизатор модели курса. Добавлено отображение количества уроков и поле вывода уроков

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
 from .models import Course, Lesson
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    def get_lessons_count(self, course):
+        return course.lessons.count()
+
+    class Meta:
+        model = Course
         fields = '__all__'
