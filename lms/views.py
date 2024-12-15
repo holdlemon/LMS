0d2bf9a38d4a3623b0957c1fe4999724827e6b38
holdlemon <d.vajov2010@yandex.ru>
2024-12-15 19:04:45 +0300

    Добавлены и настроены права

--- lms/views.py
+++ lms/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+
+from users.permissions import IsModer, IsOwner
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
@@ -7,26 +10,70 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_queryset(self):
+        """Фильтруем набор данных в зависимости от пользователя"""
+
+        if self.request.user.groups.filter(name="Модератор").exists():
+            return Course.objects.all()
+        user = self.request.user
+        return Course.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        """Устанавливает права на действия пользователя."""
+
+        if self.action in (
+                "list",
+                "retrieve",
+                "update",
+                "partial_update",
+        ):
+            permission_classes = [IsAuthenticated, IsModer | IsOwner]
+        elif self.action in ("create",):
+            permission_classes = [IsAuthenticated, ~IsModer]
+        elif self.action in ("destroy",):
+            permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModer]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class LessonListAPIView(generics.ListAPIView):
-    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModer | IsOwner]
+
+    def get_queryset(self):
+        """Фильтруем набор данных в зависимости от пользователя"""
+
+        if self.request.user.groups.filter(name="Модератор").exists():
+            return Lesson.objects.all()
+        user = self.request.user
+        return Lesson.objects.filter(owner=user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-
+    permission_classes = [IsAuthenticated, IsModer | IsOwner]
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModer | IsOwner]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
