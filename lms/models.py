c9c2e2491a1bead33a85af7eb7f3323764193cc9
holdlemon <d.vajov2010@yandex.ru>
2024-12-15 19:04:25 +0300

    Добавлены поля владельцев

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,6 +6,10 @@ class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название курса')
     preview = models.ImageField(upload_to='lms/course', blank=True, null=True, verbose_name='Превью курса')
     description = models.TextField(blank=True, null=True, verbose_name='Описание курса')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Курс'
@@ -17,6 +22,10 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lms/lesson', blank=True, null=True, verbose_name='Превью урока')
     url = models.URLField(verbose_name='Ссылка на видео')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True, related_name='lessons', verbose_name='Курс')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Урок'
