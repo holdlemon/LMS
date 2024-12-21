63668eb945b449b33f4a8742850575b1974920ab
holdlemon <d.vajov2010@yandex.ru>
2024-12-21 15:38:27 +0300

    Добавлен валидатор для проверки отсутствия сторонних ресурсов в материалах уроков

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from .models import Course, Lesson
+from .validators import validate_youtube_url
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
+    video_url = serializers.URLField(validators=[validate_youtube_url])
+
     class Meta:
         model = Lesson
         fields = '__all__'
