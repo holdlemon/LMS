ec3f0328e20351097a1512d0d4e96b4ed63829c5
holdlemon <d.vajov2010@yandex.ru>
2025-02-09 20:36:01 +0300

    Линтеры

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ValidationError
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
@@ -50,7 +49,7 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
         try:
             stripe_product = create_stripe_product(product.name)
-            stripe_product_price =  create_stripe_price(
+            stripe_product_price = create_stripe_price(
                 stripe_product, product.price
             )
             session_id, payment_link = create_stripe_session(
